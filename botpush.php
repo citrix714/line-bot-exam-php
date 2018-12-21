14e0d6f95b3aa053a894fbf24ea8903fa7a1b137
citrix714 <40513042+citrix714@users.noreply.github.com>
2018-12-22 03:58:17 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,9 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = 'xlwEv/+O1R2CN9unALEq7jfJZFLimRWdPFJ8bwMW+obDVRvBJxg8NTYf0apVcuNJ9l9HCs3POLCDFaMtZ0Bf90spftkHiP3E/vMJJ37FxdU8Xc4ej+Y6/6FQ5F+QEFPvQ3lFYy6ELVJ/Gt0PlJvWBgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'f2eab052130004c54e58f684db5f9083';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U22f1e113c6239968c5d0950b16fa0f9c';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
